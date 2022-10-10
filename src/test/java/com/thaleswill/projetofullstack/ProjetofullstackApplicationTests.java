9003ff7376f2bf8ce2fd0d1d0b269bcb72013408
thaleswillreis <thaleswill@yahoo.com.br>
2022-10-10 13:25:22 -0300

    Commit de criação do projeto do backend

---
+++ src/test/java/com/thaleswill/projetofullstack/ProjetofullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thaleswill.projetofullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetofullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
