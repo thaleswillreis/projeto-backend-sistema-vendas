c3991befd87ad69552ad12cdf9bb3c50a7b8fc33
thaleswillreis <thaleswill@yahoo.com.br>
2022-10-10 22:09:11 -0300

    Inserção das camadas 'Repositories' e 'Services' da entidade 'Categorias'

---
+++ src/main/java/com/thaleswill/projetofullstack/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.thaleswill.projetofullstack.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.thaleswill.projetofullstack.domain.Categoria;
+import com.thaleswill.projetofullstack.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) { 
+		 Optional<Categoria> obj = repo.findById(id); 
+		return obj.orElse(null);
+	}
+}
