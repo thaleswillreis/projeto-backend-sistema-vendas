d2d3613fb446b63fba1165bc94bf5ad82729420f
thaleswillreis <thaleswill@yahoo.com.br>
2022-10-18 13:22:33 -0300

    Ajuste na validação de Email (email repedtido) no método UPDATE de Cliente

--- src/main/java/com/thaleswill/projetofullstack/dto/ClienteDTO.java
+++ src/main/java/com/thaleswill/projetofullstack/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.thaleswill.projetofullstack.domain.Cliente;
+import com.thaleswill.projetofullstack.services.validation.ClienteUpdate;
 
+@ClienteUpdate //Anotação personalizada
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
---
+++ src/main/java/com/thaleswill/projetofullstack/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,52 @@
+package com.thaleswill.projetofullstack.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.thaleswill.projetofullstack.domain.Cliente;
+import com.thaleswill.projetofullstack.dto.ClienteDTO;
+import com.thaleswill.projetofullstack.repositories.ClienteRepository;
+import com.thaleswill.projetofullstack.resources.exception.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+	@Autowired
+	private HttpServletRequest request;
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+		
+		@SuppressWarnings("unchecked")
+		Map<String, String> map = (Map<String, String>) request.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null && !aux.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
