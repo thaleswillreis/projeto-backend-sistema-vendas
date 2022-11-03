6bc14890d918f430084467555a350c087d71aacb
thaleswillreis <thaleswill@yahoo.com.br>
2022-11-03 12:06:14 -0300

    Autorizacao JWT a partir do token

--- src/main/java/com/thaleswill/projetofullstack/config/SecurityConfig.java
+++ src/main/java/com/thaleswill/projetofullstack/config/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.thaleswill.projetofullstack.security.JWTAuthenticationFilter;
+import com.thaleswill.projetofullstack.security.JWTAuthorizationFilter;
 import com.thaleswill.projetofullstack.security.JWTUtil;
 
 @Configuration
@@ -65,7 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.antMatchers(PUBLIC_MATCHERS).permitAll()
 			.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-//		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
