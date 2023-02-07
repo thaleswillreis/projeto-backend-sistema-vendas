8bc0feb43c3d295616fd0394476c2abccf264271
thaleswillreis <thaleswill@yahoo.com.br>
2023-02-07 08:11:10 -0300

    Customização da tela de apresentação da documentação da API no Swagger

--- src/main/java/com/thaleswill/projetofullstack/config/SwaggerConfig.java
+++ src/main/java/com/thaleswill/projetofullstack/config/SwaggerConfig.java
@@ -1,10 +1,14 @@
 package com.thaleswill.projetofullstack.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,8 +21,19 @@ public class SwaggerConfig {
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
-				.apis(RequestHandlerSelectors.any())
+				.apis(RequestHandlerSelectors.basePackage("com.thaleswill.projetofullstack.resources"))
 				.paths(PathSelectors.any())
-				.build();
+				.build()
+				.apiInfo(apiInfo());
 	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfo("API de Sistemas de Vendas Simples",
+				"Esta API é utilizada para instrução de alunos do Minicurso de Java e Springboot",
+				"Versão 1.0",
+				"https://github.com/thaleswillreis/projeto-backend-sistema-vendas",
+				new Contact("Thales Will", "https://github.com/thaleswillreis", "thaleswillreis@gmail.com"),
+				"Permitido uso para estudantes", "https://github.com/thaleswillreis/projeto-backend-sistema-vendas", Collections.emptyList());
+	}
+
 }
